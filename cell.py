0dbdce71f9bd8efa00739075a34ca1c69e02d90b
Grant Ramsay <seapagan@gmail.com>
2023-04-13 11:44:18 +0100

    display count of mines around clicked cell
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- cell.py
+++ cell.py
@@ -20,18 +20,48 @@ class Cell:
         return f"Cell({self.x}, {self.y})"
 
     def create_btn_object(self, location):
-        btn = Button(
-            location,
-            width=12,
-            height=4,
-        )
+        btn = Button(location, width=12, height=4)
         btn.bind("<Button-1>", self.left_click_actions)
         btn.bind("<Button-3>", self.right_click_actions)
         self.cell_btn_object = btn
 
     def left_click_actions(self, event):
-        print(event)
-        print("Left CLick")
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
+
+    def get_cell_by_axis(self, x, y):
+        # return a cell object based on the value of x,y
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+    @property
+    def surrounding_cells(self):
+        cells = [
+            self.get_cell_by_axis(self.x - 1, self.y - 1),
+            self.get_cell_by_axis(self.x - 1, self.y),
+            self.get_cell_by_axis(self.x - 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y - 1),
+            self.get_cell_by_axis(self.x + 1, self.y),
+            self.get_cell_by_axis(self.x + 1, self.y + 1),
+            self.get_cell_by_axis(self.x, self.y + 1),
+        ]
+        return [cell for cell in cells if cell is not None]
+
+    @property
+    def surrounding_cells_mines_count(self):
+        return sum([1 for cell in self.surrounding_cells if cell.is_mine])
+
+    def show_cell(self):
+        self.cell_btn_object.configure(text=self.surrounding_cells_mines_count)
+
+    def show_mine(self):
+        # a logic to interrupt game and display a message that the player has
+        # lost
+        self.cell_btn_object.configure(bg="red")
 
     def right_click_actions(self, event):
         print(event)
