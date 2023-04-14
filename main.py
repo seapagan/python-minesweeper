c23b301fc014d5c94152c9fc1b881025ac263ece
Grant Ramsay <seapagan@gmail.com>
2023-04-14 09:13:24 +0100

    split menu and frames out to separate files
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from tkinter import Frame, Label, Menu, Tk
+from tkinter import Label, Tk
 
 import settings
 import utils
 from cell import Cell
+from components.frames import Frames
+from components.menu import MenuBar
 
 root = Tk()
 # override the settings of the root window
@@ -11,53 +13,34 @@ root.geometry(f"{settings.WIDTH}x{settings.HEIGHT}")
 root.title("Minesweeper")
 root.resizable(False, False)
 
-top_frame = Frame(
-    root,
-    bg="black",
-    width=settings.WIDTH,
-    height=utils.height_prct(25),
-)
-top_frame.place(x=0, y=0)
+frames = Frames(root)
+frames.place_frames()
 
 game_title = Label(
-    top_frame, text="Minesweeper Game", font=("", 30), bg="black", fg="white"
+    frames.top_frame,
+    text="Minesweeper Game",
+    font=("", 30),
+    bg="black",
+    fg="white",
 )
 game_title.place(x=utils.width_prct(25), y=0)
 
-left_frame = Frame(
-    bg="black",
-    width=utils.width_prct(25),
-    height=utils.height_prct(75),
-)
-left_frame.place(x=0, y=utils.height_prct(25))
-
-center_frame = Frame(
-    bg="black",
-    width=utils.width_prct(75),
-    height=utils.height_prct(75),
-)
-center_frame.place(x=utils.width_prct(25), y=utils.height_prct(25))
-
-menubar = Menu(root)
-filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="New Game")
-filemenu.add_separator()
-filemenu.add_command(label="Exit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
+# add the menu bar
+menubar = MenuBar(root)
 root.config(menu=menubar)
 
 for x in range(settings.GRID_SIZE):
     for y in range(settings.GRID_SIZE):
         c = Cell(x, y)
-        c.create_btn_object(center_frame)
-        c.create_btn_object(center_frame)
+        c.create_btn_object(frames.center_frame)
+        c.create_btn_object(frames.center_frame)
         c.cell_btn_object.grid(
             column=x,
             row=y,
         )
 
 # call the label from the cell class
-Cell.create_cell_count_label(left_frame)
+Cell.create_cell_count_label(frames.left_frame)
 Cell.cell_count_label_object.place(x=0, y=0)
 Cell.randomize_mines()
 
